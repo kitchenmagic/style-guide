fee018871dac9826e70d379986c5a2e0f07980d6
Josh <josh@Joshs-MacBook-Pro.local>
2018-11-05 15:09:23 -0500

    refactor gulp for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,34 @@
+// requirements
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var merge = require('merge-stream')
 
-// Compile individual SCSS files - will be used for individual modules of the website
-gulp.task('styles', function() {
-    gulp.src('styles/scss/**/*.scss')
+// define paths
+var paths = {
+    srcSCSS:     'styles/scss/**/*.scss',
+    srcMainSCSS: 'styles/scss/main.scss',
+    srcBlogSCSS: 'styles/scss/main-blog.scss',
+    srcBsgSCSS:  'styles/scss/main-bsg.scss',
+
+    distCSS:     'styles/css'
+}
+
+// compile all SCSS to CSS 
+gulp.task('compile', function(done) {
+    var hubspotModules = gulp.src(paths.srcSCSS);
+    var mainCompile = gulp.src(paths.srcMainSCSS);
+    var blogCompile = gulp.src(paths.srcBlogSCSS);
+    var bsgCompile = gulp.src(paths.srcBsgSCSS);
+
+    return merge(hubspotModules, mainCompile, blogCompile, bsgCompile)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles/css'));
+        .pipe(gulp.dest(paths.distCSS));
+    done();
 });
 
-// Compiles to main.css which will be our primary CSS file for the website
-gulp.task('main-compile', function() {
-    gulp.src('styles/scss/main.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles/css'));
+// default gulp task
+gulp.task('default', (done) => {
+    gulp.watch(paths.srcSCSS, gulp.parallel('compile'));
+    done();
 });
 
-// Compiles to main-blog.css which will be the CSS file for the primary blog
-gulp.task('blog-compile', function() {
-    gulp.src('styles/scss/main-blog.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles/css'));
-});
-
-// Compiles to  which will be the CSS file for the living style guide
-gulp.task('bsg-compile', function() {
-    gulp.src('styles/scss/main-bsg.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('styles/css'));
-});
-
-
-gulp.task('default', function() {
-    gulp.watch(['styles/scss/**/*.scss', 'styles/scss/brand-style-guide/**/*.scss' ],  ['styles', 'main-compile']);
-});
